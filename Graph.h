85435f85f5ca3948728c88b7a09a5aee00a91ff5
joseacunacarrera <51470448+joseacunacarrera@users.noreply.github.com>
2020-11-07 00:14:49 -0600

    First Commit
    First commit with some object modeling and code

---
+++ Graph.h
@@ -0,0 +1,11 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+class Graph{
+    //atributes
+    
+    //constructor
+    //methods
+}
+
+#endif GRAPH_H
---
+++ World.h
@@ -0,0 +1,19 @@
+#ifndef WORLD_H
+#define WORLD_H
+
+class World{
+    // atributes
+    Graph worldGraph;
+    int notColored;
+
+    // constructor
+    World()
+    {
+        Graph worldGraph = new Graph();
+        notColored = 0;
+    }
+    // methods
+    void startColoring()
+};
+
+#endif WORLD_H
