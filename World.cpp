6ad7a5ffcc844e404b754ce190ccf4f2a0c7ba25
Marlon_creator <54759773+Marlon-creator@users.noreply.github.com>
2020-11-26 14:05:56 -0600

    Last Dinaminc
    Update

--- World.cpp
+++ World.cpp
@@ -5,20 +5,30 @@ void World::insertCountry(Country pCountry)
     countries.push_back(pCountry);
 }
 
-void World::divideAndConquer()
+void World::divideAndConquerMarn(std::vector<Country*> Quadrant, int colors,int ini, int fin)
 {
     
+    int i=0;
+    if(i<Quadrant.size())//size=27 colors=3
+    {
+        for (int co=0; co<colors; co++){
+            divideAndConquer(1-3)
+            divideAndConquer(1-3)
+            divideAndConquer(1-3)
+        }
+    }
 }
 void World::dynamicProgramming(std::vector<Country*> Quadrant, int colors){
     int tokenColors=0;
     for (int i = 0; i < Quadrant.size(); i+colors)
     {   
         if (tokenColor=0){
-            Quadrant.at(co+colors)->setColor(co+1);
+            for (int co=0; co<colors; co++)
+                {Quadrant.at(co+1)->setColor(co+1);}
             tokenColor=1;
         }
         else{
-            for (int co=0; i<colors; co++){
+            for (int co=0; co<colors; co++){
                 if(Quadrant.at(((i+colors)->getOriginPoint())-(Quadrant.at(i)->getOriginPoint()))>20){
                     Quadrant.at(i+colors)->setColor(co+1);  
                 }
